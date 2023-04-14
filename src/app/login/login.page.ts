68ceb0cbdfc74a186f1b71a842f6b37b7d434bbb
theCanner <jcanneracierda@gmail.com>
2023-04-14 15:50:42 +0800

    form validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,37 +9,54 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  email : string = ''
-  password : string = ''
+  // email : string = ''
+  // password : string = ''
   emailInvalid : boolean = false
   disableButton :boolean = true
+  loginForm: FormGroup
+  
   constructor(
-    private router:Router
-  ) { }
+    private router:Router,
+    private formbuilder:FormBuilder
+    ) {
+      this.loginForm = this.formbuilder.group({
+        email: new FormControl('',
+                Validators.compose([
+                          Validators.required,
+                          Validators.pattern(/[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})/)
+                        ])),
+        password: new FormControl('',[Validators.required,Validators.minLength(4)]),
+      })
+     }
 
   ngOnInit() {
   }
 
 
+  get f(): any {
+    return this.loginForm.controls;
+}
+
   validateEmail(e:any){
-    if (!this.email.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))){
-      this.emailInvalid = true
-      this.validateField()
-      return
-    }else { 
-      this.emailInvalid = false
-      this.validateField()
-      return 
-    }
+    // if (!this.email.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))){
+    //   this.emailInvalid = true
+    //   this.validateField()
+    //   return
+    // }else { 
+    //   this.emailInvalid = false
+    //   this.validateField()
+    //   return 
+    // }
   }
 
   validateField(){
-    if(!this.emailInvalid && this.password.length >  0 ){
-        this.disableButton = false
-        return
-    }else {
-      this.disableButton = true
-    }
+    console.log(this.f.password.invalid  && this.f.password.touched)
+    // if(!this.emailInvalid && this.password.length >  0 ){
+    //     this.disableButton = false
+    //     return
+    // }else {
+    //   this.disableButton = true
+    // }
   }
   
 
